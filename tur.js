958dcb5576b7788f77fa6fd0362a288d5aff041e
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-03-22 10:34:53 +0100

    Update tur.js

--- tur.js
+++ tur.js
@@ -12,8 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // Initialize map
   var map = new mapboxgl.Map({
     container: "turmap",
-    //style: "mapbox://styles/dbarnesen/ckk3vfvbi4t1v17p82fuy61pt",
-    style: "mapbox://styles/dbarnesen/clu1rz6ke00qa01qw9c1k04b8",
+    style: "mapbox://styles/dbarnesen/ckk3vfvbi4t1v17p82fuy61pt",
+    //style: "mapbox://styles/dbarnesen/clu1rz6ke00qa01qw9c1k04b8",
     //style: "mapbox://styles/dbarnesen/clu1rd21q002x01pi0udm9dds",
     center: [8.2961, 59.91639], // Default center (Torvet, Risør)
     zoom: 5.5,
